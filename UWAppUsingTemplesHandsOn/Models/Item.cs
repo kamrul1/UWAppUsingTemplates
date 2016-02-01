1d882a7ffb92a55d4e43cd5735fe35a891dee845
kamrul i <to.kamrul@outlook.com>
2016-02-01 01:36:46 +0000

    Hands on with Datatemplates

---
+++ UWAppUsingTemplesHandsOn/Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace UWAppUsingTemplesHandsOn.Models
+{
+    public class Item
+    {
+        public string Title { get; set; }
+
+        public Color SymbolColor { get; set; }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
---
+++ UWAppUsingTemplesHandsOn/ViewModels/ItemsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UWAppUsingTemplesHandsOn.Models;
+using Windows.UI;
+
+namespace UWAppUsingTemplesHandsOn.ViewModels
+{
+    public class ItemsViewModel
+    {
+        public List<Item> Items { get; set; }
+
+        public ItemsViewModel()
+        {
+            Items = new List<Item>
+            {
+                new Item {Title="Denver", SymbolColor=Colors.Red }
+                , new Item {Title="Easy" , SymbolColor = Colors.Green }
+                , new Item { Title="Frank", SymbolColor = Colors.Blue }
+
+            };
+        }
+    }
+}
